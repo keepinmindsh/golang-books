f626073ead0e613b51b8b5da434403fb573b7686
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-08-01 22:42:04 +0900

    golang books - samples

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang-books/basic"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+
+	basic.GoStringCannotHaveNil()
+
+	basic.GoNilWithObject()
+
+	a, b := basic.Swap("Hello", "World")
+	fmt.Println(a, b)
+
+	x, y := basic.Split(50)
+	fmt.Println(x, y)
+
+	basic.ValueCheck()
+
+	basic.Print()
+
+	basic.TestPointer()
 }
