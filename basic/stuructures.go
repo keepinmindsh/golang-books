f1d287d5bdf46126ce620a74fe054cb5dcda44cf
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-08-02 23:50:52 +0900

    golang - books for stuct

---
+++ basic/stuructures.go
@@ -0,0 +1,11 @@
+package basic
+
+import "time"
+
+type StuructureSample struct {
+	ID        int64     `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+}
